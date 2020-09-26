6ceec909b14d06fbdc081feb630cefdb19d265c3
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2020-09-27 01:31:21 +0530

    Update twitter handle

--- plugins/aganvee/src/components/footer.tsx
+++ plugins/aganvee/src/components/footer.tsx
@@ -10,6 +10,20 @@ const Footer = () => {
 
   return (
     <Box as="footer" variant="footer">
+      {/*<button
+        sx={{
+          variant: `buttons.toggle`,
+          fontWeight: `semibold`,
+          display: `block`,
+          mx: `auto`,
+          mb: 3,
+        }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>*/}
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       <br />
       <Flex
